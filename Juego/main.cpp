85b34407d3b5cc648892384f6634d6e16c274513
jmea3911 <129397108+jmea3911@users.noreply.github.com>
2025-05-15 20:35:44 -0600

    Initial Commit

---
+++ Juego/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Juego
+//
+//  Created by Juan Manuel Alvarado on 15/5/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
